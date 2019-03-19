cb601364986c98cf0f08a7c4aef42c182d3801a4
theo lemoine <theo.lemoine0@gmail.com>
2019-03-19 23:19:11 +0100

    pour creer des rams, utilisez la ligne de comande dans la root du projet : php create-ram login password

--- create-ram.php
+++ create-ram.php
@@ -2,17 +2,14 @@
 
 require_once 'libs/models/Ram.php';
 
-$login = 'RAM test';
-$mdp = 'test';
-
 $pdo = new PDO('mysql:dbname=bubble-agency;host=127.0.0.1;charset=utf8', 'root', '');
 
 $stm = $pdo->prepare('INSERT INTO ram (login, mdp) VALUES(:login, :mdp)');
 
-$hashed_mdp = password_hash($mdp, PASSWORD_DEFAULT);
+$hashed_mdp = password_hash($argv[2], PASSWORD_DEFAULT);
 
 $ok = $stm->execute([
-    ':login' => $login,
+    ':login' => $argv[1],
     ':mdp' => $hashed_mdp,
 ]);
 
